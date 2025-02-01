2351be6dfa338436fa2aeedb178b9e9c7a2b9ee4
ShaineRosewel <shainematala@gmail.com>
2025-02-02 01:02:20 +0800

    initial commit

---
+++ r_scripts/generate_letter.R
@@ -0,0 +1,144 @@
+library(rmarkdown)
+library(magrittr)
+library(officer)
+
+get_last_word <- function(sentence) {
+  words <- unlist(strsplit(sentence, "\\s+"))
+  return(tail(words, 1))
+}
+
+get_first_word <- function(sentence) {
+  words <- unlist(strsplit(sentence, "\\s+"))
+  return(head(words, 1))
+}
+
+library(officer)
+
+generate_letter2 <- function(your_name,
+                            your_address_line1,
+                            your_address_line2,
+                            your_contact,
+                            your_email,
+                            date,
+                            recipient_name,
+                            recipient_title,
+                            office_address_line1,
+                            office_address_line2,
+                            body) {
+  greeting <- paste("Mahal na",
+                    get_first_word(recipient_name),
+                    paste0(get_last_word(recipient_name),","))
+  
+  closing <- "Taos-pusong gumagalang,"
+  
+  property_bold <- fp_text(bold = TRUE, font.family = "Arial")
+  property <- fp_text(font.family = "Arial")
+  
+  doc <- read_docx()
+  
+  doc <- body_add(doc, fpar(ftext(your_name, property_bold)))
+  doc <- body_add(doc, fpar(ftext(your_address_line1, property)))
+  doc <- body_add(doc, fpar(ftext(your_address_line2, property)))
+  doc <- body_add(doc, fpar(ftext(your_contact, property)))
+  doc <- body_add(doc, fpar(ftext(your_email, property)))
+  doc <- body_add(doc, fpar())
+  doc <- body_add(doc, fpar(ftext(date, property)))
+  doc <- body_add(doc, fpar())
+  doc <- body_add(doc, fpar())
+  doc <- body_add(doc, fpar(ftext(recipient_name, property_bold)))
+  doc <- body_add(doc, fpar(ftext(recipient_title, property)))
+  doc <- body_add(doc, fpar(ftext(office_address_line1, property)))
+  doc <- body_add(doc, fpar(ftext(office_address_line2, property)))
+  doc <- body_add(doc, fpar())
+  doc <- body_add(doc, fpar(ftext(greeting, property)))
+  doc <- body_add(doc, fpar())
+  
+  for (item in body) {
+    doc <- body_add(doc, fpar(ftext(item, property), fp_p = fp_par(text.align = "justify")))
+    
+    doc <- body_add(doc, fpar())
+  }
+
+  doc <- body_add(doc, fpar())
+  doc <- body_add(doc, fpar(ftext(closing, property)))
+  doc <- body_add(doc, fpar())
+  doc <- body_add(doc, fpar())
+  doc <- body_add(doc, fpar(ftext(your_name, property)))
+  print(doc, target = "letter.pdf")
+}
+
+generate_letter_pdf <- function(
+                                isodate,
+                                your_name,
+                                your_address_line1,
+                                your_address_line2,
+                                your_contact,
+                                your_email,
+                                date,
+                                recipient_name,
+                                recipient_title,
+                                office_address_line1,
+                                office_address_line2,
+                                body) {
+  folder_path <- paste(isodate, "letters", sep="_")
+  
+  # Check if the folder exists, and if not, create it
+  if (!file.exists(folder_path)) {
+    dir.create(folder_path)
+    cat("Folder created at:", folder_path)
+  } else {
+    cat("Folder already exists at:", folder_path)
+  }
+  
+  letter_content <- paste0(
+    "---\n",
+    "title: \"\"\n",
+    "output:\n",
+    "  pdf_document: default\n",
+    "  word_document: default\n",
+    "header-includes:\n",
+    "  - \\usepackage{graphicx}\n",  # Enables image insertion
+    "  - \\usepackage{adjustbox}\n", # Allows size and alignment adjustments
+    "  - \\usepackage{parskip}\n",    # Adds spacing between paragraphs
+    "  - \\setlength{\\parskip}{1em}\n", # Sets paragraph spacing to 1em
+    "---\n\n",
+    
+    "**", your_name, "**  \n",
+    your_address_line1, "  \n",
+    your_address_line2, "  \n",
+    your_contact, "  \n",
+    your_email, "  \n\n",
+    
+    date, "   \n\n",
+    
+    "\\vspace{1.5em}\n\n",
+    
+    "**", recipient_name, "**  \n",
+    recipient_title, "  \n",
+    office_address_line1, "  \n",
+    office_address_line2, "  \n\n",
+    
+    paste("Mahal na",
+          get_first_word(recipient_name),
+          paste0(get_last_word(recipient_name),",\n\n")),
+    
+    paste(body, collapse = "\n\n"), "\n\n",
+  
+    "\\vspace{1.5em}\n\n",
+    
+    "Taos-pusong gumagalang,\n\n",
+    "\\vspace{-1em}\n\n",
+    "\\noindent\\includegraphics[width=1.5in]{", "signature.png", "}\n\n",
+    
+    "\\vspace{-2em}\n\n",
+    
+    your_name
+  )
+  
+  
+  writeLines(letter_content, "letter.Rmd")
+  
+  render("letter.Rmd", output_format = "pdf_document", output_file = paste(
+    paste0(folder_path,"/",isodate), get_first_word(recipient_name), get_last_word(recipient_name), sep = "_")
+  )
+}
